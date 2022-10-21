cf302586f362b32097778fc518c2fee148395fe6
Danny <ddiaz@scu.edu>
2022-10-20 20:52:02 -0700

    Create functions

--- js-file.js
+++ js-file.js
@@ -7,27 +7,32 @@ div.classList.add('content');
 
 
 
-for (let i = 0; i< 16; i++)
-{
-    const div = document.createElement('div');
-    div.classList.add('content');
-    container.appendChild(div);
-}
+/*Creates boxes in a column */
 
+createColumns(container);
 let columns = document.querySelectorAll('.content');
-
 columns.forEach(column => createBoxes(column));
 
-function createBoxes(column){
-    for (let j = 0; j<16; j++){
+
+
+/* Functions */
+
+function createColumns(container)
+{
+    for (let i = 0; i< 16; i++)
+    {
+        const div = document.createElement('div');
+        div.classList.add('content');
+        container.appendChild(div);
+    }
+}
+
+function createBoxes(column)
+{
+    for (let j = 0; j<16; j++)
+    {
     const div = document.createElement('div');
     div.classList.add('box');
     column.appendChild(div);
     }
 }
-/*for (let j = 0; j <16; j++){
-
-    const div = document.createElement('div');
-    div.classList.add('box');
-    column.appendChild(div);
-}*/
